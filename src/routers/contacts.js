4764895214e1f3ae80cf8234d3918a50c2c7d42f
RomaSamoilenko <107999000+RomaSamoilenko@users.noreply.github.com>
2024-06-29 19:41:25 +0300

    isValidId_add_router

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -9,11 +9,15 @@ import {
 } from '../controllers/controllers.js';
 import { validateBody } from '../middlewares/validateBody.js';
 import { createContactSchema, updateContactSchema } from "../validation/contacts.js";
+import { isValidId } from '../middlewares/isValidId.js';
 
 const router = Router();
+
+router.get('/contacts/:contactId',  isValidId);
+
 router.get('/contacts', ctrlWrapper(getContactsController));
 
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+router.get('/contacts/:contactId',  ctrlWrapper(getContactByIdController));
 
 router.post('/contacts', validateBody(createContactSchema), ctrlWrapper(createContactController));
 
