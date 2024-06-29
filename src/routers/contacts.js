05dded9ec20d9d057328e974879ee7ccc3fd8315
RomaSamoilenko <107999000+RomaSamoilenko@users.noreply.github.com>
2024-06-29 21:17:58 +0300

    isValidId(), ctrlWrapper(getContactByIdController));

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -13,11 +13,11 @@ import { isValidId } from '../middlewares/isValidId.js';
 
 const router = Router();
 
-router.get('/contacts/:contactId',  isValidId);
+router.get('/contacts/:contactId',  isValidId ());
 
 router.get('/contacts', ctrlWrapper(getContactsController));
 
-router.get('/contacts/:contactId',  ctrlWrapper(getContactByIdController));
+router.get('/contacts/:contactId',  isValidId(), ctrlWrapper(getContactByIdController));
 
 router.post('/contacts', validateBody(createContactSchema), ctrlWrapper(createContactController));
 
