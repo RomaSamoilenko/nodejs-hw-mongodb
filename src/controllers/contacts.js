3a0100a005a2ee8cadc7d8c34bee7fa622454eb0
RomaSamoilenko <107999000+RomaSamoilenko@users.noreply.github.com>
2024-07-13 19:10:52 +0300

    email+images pull

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,12 +1,11 @@
-import { getAllContacts, 
-         getContactById, 
-         createContact, 
-         updateContact, 
-         deleteContact } from '../services/contacts.js';
+import { getAllContacts, getContactById, createContact, updateContact, deleteContact } from '../services/contacts.js';
 import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 import { parseSortParams } from '../utils/parseSortParams.js';
 import { parseFilterParams } from '../utils/parseFilterParams.js';
 import createHttpError from 'http-errors';
+import { saveFileToUploadDir } from '../utils/saveFileToUploadDir.js';
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
+import { env } from '../utils/env.js';
 
 export const getContactsController = async (req, res) => {
     const userId = req.user._id; 
@@ -55,7 +54,21 @@ export const createContactController = async (req, res) => {
 
 export const patchContactController = async (req, res, next) => {
     const { contactId } = req.params;
-    const result = await updateContact(contactId, req.body);
+    const photo = req.file;
+
+    let photoUrl;
+
+    if (photo) {
+        if (env('ENABLE_CLOUDINARY') === 'true') {
+            photoUrl = await saveFileToCloudinary(photo);
+        } else {
+            photoUrl = await saveFileToUploadDir(photo);
+        }
+    }
+    const result = await updateContact(contactId, {
+        ...req.body,
+        photo: photoUrl,
+    });
 
     if(!result) {
         next(createHttpError(404, "Contact not found"));
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,16 +1,18 @@
 import { Router } from "express";
 import { 
-  getContactsController, 
-  getContactByIdController, 
-  createContactController, 
-  patchContactController, 
-  deleteContactController 
+    getContactsController, 
+    getContactByIdController, 
+    createContactController, 
+    patchContactController, 
+    deleteContactController 
 } from "../controllers/contacts.js";
 import { validateBody } from '../middlewares/validateBody.js';
 import { createContactSchema, updateContactSchema } from "../validation/contacts.js";
 import { authenticate } from '../middlewares/authenticate.js';
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 import { checkUserId } from '../middlewares/checkUserId.js';
+
+import { upload } from '../middlewares/multer.js';
 
 const router = Router();
 router.use(authenticate);
@@ -25,12 +27,14 @@ router.get('/:contactId',
 );
 
 router.post('/', 
+    upload.single('photo'),
     validateBody(createContactSchema), 
     ctrlWrapper(createContactController)
 );
 
 router.patch('/:contactId', 
     checkUserId,
+    upload.single('photo'),
     validateBody(updateContactSchema), 
     ctrlWrapper(patchContactController)
 );
